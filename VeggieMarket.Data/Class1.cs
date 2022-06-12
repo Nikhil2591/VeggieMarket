9b6f6f3545ed71e03e0fdaca4e808ac6c8fc1801
Nikhil2591 <Nikhil_369@hotmail.co.uk>
2022-06-12 22:51:48 +0100

    Initial Commit

---
+++ VeggieMarket.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VeggieMarket.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ VeggieMarket.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VeggieMarket.Services
+{
+    public class Class1
+    {
+    }
+}
